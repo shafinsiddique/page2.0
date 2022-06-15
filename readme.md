69ceda3798638605be0182d6dd74afbd79389bdf
Shafin Siddique <shafinsiddique@fb.com>
2022-06-14 20:38:56 -0700

    demo up.

---
+++ readme.md
@@ -0,0 +1,62 @@
+## Welcome to Page2.0
+
+### A Lisp Interpreter Written in OCaml.
+Yes, I really like Lisp, and interpreters .. and OCaml now too. The interpreter uses a recursive descent parser. Here's a short demo:
+
+![Lisp Demo](./demo/page2.0-demo.gif)
+
+
+### Documentation
+
+##### 1. Lists
+
+```
+> (list 1 2 3) // define list.
+(list 1 2 3)
+
+> (car (list 1 2 3)) // get first element
+1
+
+> (cdr (list 4 5 6)) // get rest
+(list 5 6)
+
+> (cons 1 (list 4 5 6)) // construct new list with elements combined
+(list 1 4 5 6)
+
+> (length (list 1 2 3))
+3
+```
+
+
+
+##### 3. Strings
+
+```
+> (length "hello")
+5
+
+> (equals "hello" "world")
+False
+
+> "Hello World"
+Hello World
+```
+
+##### 3. Numbers
+
+```
+> 10
+10
+
+> (+ 5 5 )
+10
+
+> (% 4 2)
+0
+
+> (/ 4 2)
+2
+
+> (* 2 2)
+4
+```
--- src/bin/main.ml
+++ src/bin/main.ml
@@ -3,18 +3,18 @@ open Parser
 (* open Tokens *)
 open Printf
 open Evaluator
-let tokens = tokenize "(length (list (+ 2 4) 3 4 100))"
 
-let _ = match tokens with
-    | Ok tokens_list -> let nodes = (parse_expression tokens_list) in
-        begin
-        match nodes with
-            | Ok (ast, _) -> evaluate [ast]
-            |_ -> printf "Error Parsing"
-        end
-    | _ -> printf "Error Tokenizing"
+let () = printf "Welcome to Page 2.0: A Lisp Interpreter written in OCaml.\nVersion : 1.0\nBuilt By: Shafin\n\n"
 
+let rec repl_loop ()=
+    let _ = printf "> " in
+    let input_str = read_line () in
+    let tokens = tokenize input_str in
+    let _ = match tokens with
+        | Ok tokens_list -> let nodes = (parse_expression tokens_list) in
+            (match nodes with
+                | Ok (ast, _) -> evaluate [ast]; printf "\n"; repl_loop ()
+                | Error e -> printf "%s\n" e)
+        | _ -> printf "Error Tokenizing." in ()
 
-(* let _ = match tokens with
-    | Ok tokens ->  List.iter print_token tokens
-    | _ -> printf "err" *)
+let () = repl_loop ()
